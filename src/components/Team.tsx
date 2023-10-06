1f1c7bfc8319248a0c724f30aca2770bb8d2e328
Larry Ntori <l.ntori@generisdevelopers.com>
2023-10-06 11:16:47 +0000

    fixes

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,6 +1,8 @@
 import Joseph from "../assets/Joseph Martey Dickson.jpeg";
-import Michael from "../assets/Michael Gyamfi.jpeg";
-import Dickson from "../assets/Dickson Addai-Badu Jr.jpeg";
+import Michael from "../assets/michael g.jpeg";
+import Dickson from "../assets/dickson.jpeg";
+import Jackson from "../assets/micheal owusu Jackson.jpeg";
+
 export const Team = () => {
   return (
     <div className="max-w-7xl px-4 py-10 sm:px-6 lg:px-8 lg:py-20 mx-auto">
@@ -14,7 +16,7 @@ export const Team = () => {
         </p>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-2">
         <div className="text-center">
           <img
             className="rounded-xl sm:w-48 sm:h-48 lg:w-64 lg:h-72 mx-auto "
@@ -27,6 +29,22 @@ export const Team = () => {
             </h3>
             <p className="text-xs text-gray-600 sm:text-sm lg:text-base">
               President and CEO
+            </p>
+          </div>
+        </div>
+
+        <div className="text-center">
+          <img
+            className="rounded-xl sm:w-48 sm:h-48 lg:w-64 lg:h-72 mx-auto"
+            src={Dickson}
+            alt=""
+          />
+          <div className="mt-2 sm:mt-4">
+            <h3 className="text-sm font-medium text-gray-800 sm:text-base lg:text-lg ">
+              Dickson Addai-Badu Jr
+            </h3>
+            <p className="text-xs text-gray-600 sm:text-sm lg:text-base">
+              Director of Operation/Projects
             </p>
           </div>
         </div>
@@ -50,15 +68,15 @@ export const Team = () => {
         <div className="text-center">
           <img
             className="rounded-xl sm:w-48 sm:h-48 lg:w-64 lg:h-72 mx-auto"
-            src={Dickson}
+            src={Jackson}
             alt=""
           />
           <div className="mt-2 sm:mt-4">
             <h3 className="text-sm font-medium text-gray-800 sm:text-base lg:text-lg ">
-              Dickson Addai-Badu Jr.
+              Michael Owusu Jackson
             </h3>
             <p className="text-xs text-gray-600 sm:text-sm lg:text-base">
-              Director of Operation/Projects
+              ICT Director
             </p>
           </div>
         </div>
